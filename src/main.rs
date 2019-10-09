5f217b80855f9ab56163cb37fb961334680fd1d9
Cokemonkey11 <Cokemonkey11@gmail.com>
2019-10-09 22:18:18 +0100

    fixup scraper for latest github topics redesign

--- src/main.rs
+++ src/main.rs
@@ -20,26 +20,25 @@ fn dump_dependencies() -> Result<(), Error> {
     ).find(Name("article"))
     .into_iter()
     .try_for_each(|repo| -> Result<(), Error> {
-        let container = repo
-            .find(Class("topics-row-container"))
-            .next()
-            .ok_or(ChillupError::NoneError{ el: "topics-row-container".into() })?;
-
-        if container.find(Class("topic-tag"))
+        if repo.find(Class("topic-tag"))
             .into_iter()
             .find(|node| node.text().trim() == "dependency")
             .is_some()
         {
-            let anchor      = repo.find(Class("f3").descendant(Name("a")))
+            let anchor = repo.find(Class("f3").descendant(Name("a")))
                 .into_iter()
-                .next()
+                .nth(1)
                 .ok_or(ChillupError::NoneError{ el: "a".into() })?;
             let description = repo.find(Class("text-gray"))
                 .into_iter()
                 .next()
                 .ok_or(ChillupError::NoneError{ el: "text-gray".into() })?;
             
-            println!("https://github.com{}\t{}", anchor.attr("href").ok_or(ChillupError::NoneError{ el: "href".into() })?, description.text().trim());
+            println!(
+                "https://github.com{}\t{}",
+                anchor.attr("href").ok_or(ChillupError::NoneError{ el: "href".into() })?,
+                description.text().trim()
+            );
         }
 
         Ok(())
